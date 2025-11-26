45f87cfbcae4d21e9cda542fcd05642d728c0aee
Sigmund Frost <sigmafi.tech@gmail.com>
2025-11-26 21:41:16 +0100

    Minor fixes to include the icon into release folder

--- src/main/window.ts
+++ src/main/window.ts
@@ -1,11 +1,32 @@
-import { BrowserWindow, Menu, ipcMain } from 'electron';
+import { BrowserWindow, Menu, ipcMain, app } from 'electron';
 import path from 'path';
 
 export const createWindow = (): BrowserWindow => {
+  let iconPath: string;
+  
+  if (app.isPackaged) {
+    
+    if (process.platform === 'win32') {
+      iconPath = path.join(process.resourcesPath, 'app.asar.unpacked', 'build', 'icons', 'win', 'icon.ico');
+    } else if (process.platform === 'darwin') {
+      iconPath = path.join(process.resourcesPath, 'build', 'icons', 'mac', 'icon.icns');
+    } else {
+      iconPath = path.join(process.resourcesPath, 'build', 'icons', 'png', '512x512.png');
+    }
+  } else {
+    if (process.platform === 'win32') {
+      iconPath = path.join(__dirname, '../../build/icons/win/icon.ico');
+    } else if (process.platform === 'darwin') {
+      iconPath = path.join(__dirname, '../../build/icons/mac/icon.icns');
+    } else {
+      iconPath = path.join(__dirname, '../../build/icons/png/512x512.png');
+    }
+  }
+
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, '../../build/icon.png'),
+    icon: iconPath,
     webPreferences: {
       preload: path.join(__dirname, '../preload/index.js'),
       nodeIntegration: false,
